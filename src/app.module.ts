8c33ea9c43114e8ae8f6d97a273858547b47cdeb
JungJin Moon <jjmoon4682@gmail.com>
2022-11-14 11:17:35 +0900

    feat: typeorm 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { Users } from './entities/Users';
+import { Boss } from './entities/Boss';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/ormconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    Users,
+    Boss,
+    TypeOrmModule.forRoot(typeORMConfig),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
